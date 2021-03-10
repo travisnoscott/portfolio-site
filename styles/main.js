a8e92a769acf82c425f299d3211626850e11d693
travisnoscott <78929901+travisnoscott@users.noreply.github.com>
2021-03-10 14:17:21 -0500

    adding comments

--- styles/main.js
+++ styles/main.js
@@ -7,6 +7,44 @@
   let firstname = "Travis";
   console.log(firstname);
 
+/* Assignment button 1*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+
+  /*Assignment button 2*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 3*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 4*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 5*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 6*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 7*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 8*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 9*/
+  function comingsoon() {
+    alert("This assignment will be coming soon");
+  }
+  /* Assignment button 10*/
   function comingsoon() {
     alert("This assignment will be coming soon");
   }
